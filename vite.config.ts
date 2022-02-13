7a659aa0a9c60ef2d1215f4b9f4b11a79ddf99ba
Samuel Alev <samuel.alev@protonmail.com>
2022-02-13 23:04:18 +0100

    fix: remove last sideeffect

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
             name: "ParcedUI",
         },
         rollupOptions: {
-            external: ["vue", "@heroicons/vue/outline", "@parced/fonts", "focus-trap", "focus-trap-vue", "uid"],
+            external: ["vue", "@heroicons/vue/outline", "@parced/fonts", "focus-trap", "focus-trap-vue", "uid/single"],
             output: {
                 assetFileNames: (chunkInfo: PreRenderedAsset): string => {
                     if (chunkInfo.name === "style.css") {
